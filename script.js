ff057a284c98f5656132f2f7a3c16fc41d6742e8
Godwin-Chinda <godwinbadubulachim@gmail.com>
2025-05-20 09:39:22 +0100

    Scripted the slider container

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const slider = document.getElementById("slider");
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 
+// Width to scroll per click (you can adjust this)
+const scrollAmount = 300;
+
+leftArrow.addEventListener("click", () => {
+  slider.scrollBy({
+    left: -scrollAmount,
+    behavior: "smooth",
+  });
+});
+
+rightArrow.addEventListener("click", () => {
+  slider.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+});
+
 //for the faq section
 function toggleFAQ(el) {
   const answer = el.nextElementSibling;
